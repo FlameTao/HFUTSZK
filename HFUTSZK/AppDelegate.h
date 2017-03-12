235a3a34b5d3610663f863a0e9f13da5ec5f3d45
flametao <flametao@126.com>
2017-03-12 13:47:27 +0800

    Initial Commit

---
+++ HFUTSZK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HFUTSZK
+//
+//  Created by Flame on 2017/3/12.
+//  Copyright © 2017年 HFUTStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HFUTSZK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HFUTSZK
+//
+//  Created by Flame on 2017/3/12.
+//  Copyright © 2017年 HFUTStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
